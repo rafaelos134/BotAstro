96778545dec0a872657344fef6ed1845fea90448
rafael <rafa_an_jo@hotmail.com>
2022-07-28 13:45:55 -0300

    enviando ao repositorio

---
+++ tt_astronomico.py
@@ -0,0 +1,50 @@
+import tweepy 
+import time 
+
+
+#tweepy
+auth = tweepy.OAuthHandler('{}'.format(KEY_1),'{}'.format(KEY_2))
+auth.set_access_token('{}'.format(KEY_3),'{}'.format(KEY_4))
+api = tweepy.API(auth,wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+user = api.me()
+
+print("\n")
+print ('pai on')
+semnada = 'não tem nd hoje'
+
+
+
+#POSTAGEM
+def sites():
+        while True:
+            with open( 'datasCalendar.txt', 'r',encoding = 'utf8') as x:
+                data = time.localtime()
+                for y in x:
+                    if data[3] == 13:
+                        oi = y.split('.')
+                        poeo = oi[2]
+                        if int (oi[0]) == int(data[1]) and int (oi [1]) == int(data[2]):
+                            print (poeo)
+                            api.update_status((poeo))
+                            print ('tudo ok parceiro')
+
+            time.sleep(3600)
+            print ("again")      
+            
+
+
+
+def local ():  
+    with open( 'datasCalendar.txt', 'r', encoding = 'utf8') as x:
+        for y in x: 
+            data = time.localtime()
+            oi = y.split('.')
+            if int (oi[0]) == int(data[1]) and int (oi [1]) == int(data[2]):  
+                print('entrou') 
+                poeo = oi[2]
+                print (poeo)
+                api.update_status((poeo))
+                print ('tudo ok parceiro') 
+
+local()
+
